d01f3e77cfb8938ba8e825113834d68df7a6febb
simanun <simare5527@gmail.com>
2023-12-30 01:42:34 +0900

    FactoryBotの記述を編集

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname { Gimei.name.first.kanji }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name { Gimei.name.last.kanji }
+    first_name { Gimei.name.first.kanji }
+    last_name_kana { Gimei.name.last.katakana }
+    first_name_kana { Gimei.name.first.katakana }
+    birthday { Faker::Date.birthday(min_age: 18, max_age: 65) }
+  end
+end
