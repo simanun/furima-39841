88d7e86887af490ca22c105321167df4c1db6809
simanun <simare5527@gmail.com>
2023-12-30 17:42:58 +0900

    passwordカラムを簡潔に修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Gimei.name.first.kanji }
     email { Faker::Internet.email }
-    password { "'1a' + Faker::Internet.password(min_length: 6)" }
+    password { "1a#{Faker::Internet.password(min_length: 6)}" }
     password_confirmation { password }
     last_name { Gimei.name.last.kanji }
     first_name { Gimei.name.first.kanji }
