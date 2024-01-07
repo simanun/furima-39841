746e9e6a58802ef7344c42e0d65cd88b34722f89
simanun <simare5527@gmail.com>
2024-01-07 18:10:54 +0900

    JavaScriptを使用した手数料、利益の計算

---
+++ app/javascript/price.js
@@ -0,0 +1,15 @@
+window.addEventListener('turbo:load', () => {
+
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    const taxInput = document.getElementById("add-tax-price");
+    taxInput.innerHTML = (Math.floor(inputValue*0.1));
+
+    const profitInput = document.getElementById("profit");
+    const taxValue = inputValue*0.1
+    profitInput.innerHTML = (Math.floor(inputValue - taxValue));
+  })
+
+});
