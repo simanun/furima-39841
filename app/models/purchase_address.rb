73bf983fc29355209195451880c9fef3cff38fd4
simanun <simare5527@gmail.com>
2024-01-20 02:06:16 +0900

    エラーが表示されるように実装

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,22 +1,27 @@
 class PurchaseAddress
   include ActiveModel::Model
   
-  attr_accessor :user_id, :item, :post_code, :prefecture_id, :city, :town_number, :building_name, :telephone_number
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :town_number, :building_name, :telephone_number
 
   with_options presence: true do
     validates :user_id
-    validates :item
+    validates :item_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end  
+    
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  with_options presence: true do
     validates :city
     validates :town_number
-    validates :telephone_number, numericality: { only_integer: true, greater_than_or_equal_to: 10_000_000_00, less_than: 10_000_000_000 }
+    validates :telephone_number, numericality: { only_integer: true },
+                                 format: { with: /\A\d{10,11}\z/, message: "is too short or too long" }
   end
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
 
 
   def save
-    purchase = Purchase.create(user_id: user_id, item: item)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, town_number: town_number, building_name: building_name, telephone_number: telephone_number, purchase_id: purchase.id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, town_number: town_number, building_name: building_name, telephone_number: telephone_number, purchase_id: purchase.id)
   end
 end
