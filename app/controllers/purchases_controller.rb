e8d8136a48f0b37118347c0050858f6b6fa5cfcc
simanun <simare5527@gmail.com>
2024-01-20 02:05:39 +0900

    フォームの情報を保存できるように実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,24 @@
 class PurchasesController < ApplicationController
   def index
-    @purchase_address = Purchase_address.all
+    @purchase_address = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
+
   end
 
   def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    @item = Item.find(params[:item_id])
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :town_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
