6c1afaee76a6546f644a503fbf4ce129668e1409
simanun <simare5527@gmail.com>
2024-01-20 02:06:33 +0900

    テストコードの実装

---
+++ spec/factories/purchase_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :purchase_address do
+    post_code { '111-1111' }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    city { Gimei.city.kanji }
+    town_number { Gimei.town.to_s }
+    building_name { Faker::Lorem.sentence }
+    telephone_number { 11111111111 }
+    
+  end
+end
