af9a3e28fcb599534b23581a1162122614b6c3af
simanun <simare5527@gmail.com>
2024-01-20 20:56:53 +0900

    決済機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,6 @@
 class PurchasesController < ApplicationController
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @purchase_address = PurchaseAddress.new
     @item = Item.find(params[:item_id])
 
@@ -8,10 +9,13 @@ class PurchasesController < ApplicationController
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
     @item = Item.find(params[:item_id])
+    amount = @item.price
     if @purchase_address.valid?
+      pay_item(amount)
       @purchase_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -19,6 +23,16 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :town_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :town_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+
+  def pay_item(amount)
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: amount,  # 商品の値段
+      card: purchase_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+
 end
--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,15 +1,16 @@
 window.addEventListener('turbo:load', () => {
 
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
+  if (priceInput) {
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
 
-    const taxInput = document.getElementById("add-tax-price");
-    taxInput.innerHTML = (Math.floor(inputValue*0.1));
+      const taxInput = document.getElementById("add-tax-price");
+      taxInput.innerHTML = (Math.floor(inputValue*0.1));
 
-    const profitInput = document.getElementById("profit");
-    const taxValue = inputValue*0.1
-    profitInput.innerHTML = (Math.floor(inputValue - taxValue));
-  })
-
+      const profitInput = document.getElementById("profit");
+      const taxValue = inputValue*0.1
+      profitInput.innerHTML = (Math.floor(inputValue - taxValue));
+    })
+  }
 });
